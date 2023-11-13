c92e1104c4046c2cd361f58fc9d464af6cc283bf
mtalirfan <mtalirfan@gmail.com>
2023-11-13 19:45:27 +0500

    bun serve

--- index.ts
+++ index.ts
@@ -5,4 +5,23 @@ function add(number:number , number2:number) {
 const num : number = 5
 const num2 : number = 10
 
-console.log(add(num, num2))
+const sum : number = add(num, num2)
+
+const server = Bun.serve({
+    port: process.env.PORT || 3000,
+    fetch(req: Request): Response | Promise<Response> {
+      const url = new URL(req.url)
+
+      if (url.pathname === '/') {
+        return new Response(`The sum of two numbers is: ${sum}`);
+      };
+      if (url.pathname === '/about') {
+        return new Response(`About Me`);
+      };
+
+      return new Response(`404!`)
+    },
+  
+  });
+
+console.log(`Listening on PORT ${server.port} at URL http://localhost:${server.port}`)
